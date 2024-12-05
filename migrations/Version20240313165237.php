0567a4397bcab76ce1766a3dbecbeea9a4ac75e2
A.Volik <volik@retailcrm.ru>
2024-12-05 21:24:24 +0300

    add exception listener, docker containers always restart policy

--- migrations/Version20240313165237.php
+++ migrations/Version20240313165237.php
@@ -24,6 +24,7 @@ final class Version20240313165237 extends AbstractMigration
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL80Platform'."
         );
 
+        $this->addSql('CREATE DATABASE IF NOT EXISTS `stage` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci');
         $this->addSql('CREATE TABLE district (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, all_population INT NOT NULL, male_population INT NOT NULL, female_population INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->abortIf(
             !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL80Platform,
